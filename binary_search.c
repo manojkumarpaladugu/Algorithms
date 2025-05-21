d9804b316b23fee6ab6941f18c5dc31b42dbb05c
Manoj Kumar Paladugu <paladugumanojkumar@gmail.com>
2025-05-21 23:11:26 +0530

    Update binary_search.c

--- binary_search.c
+++ binary_search.c
@@ -4,17 +4,17 @@
 uint32_t binary_search(uint8_t arr[], uint32_t size, uint32_t target)
 {
     // Pre-requisite: Binary search requires array to be sorted in ascending order.
-    uint32_t found_idx = -1;
-    uint32_t first_idx =0;
-    uint32_t last_idx = size - 1;
+    uint32_t left_idx = 0;
+    uint32_t right_idx = size - 1;
+    uint32_t found_idx = 0;
     
-    // Repeat until the first and last pointers meet each other
-    while(first_idx <= last_idx)
+    // Repeat until the left and right pointers meet each other
+    while(left_idx <= right_idx)
     {
         // Divide the array into two halves
-        uint32_t number_of_elements = last_idx - first_idx;
-        uint32_t middle_idx = first_idx + (number_of_elements / 2);
-        printf("first: %d, mid = %d, last: %d\n", first_idx, middle_idx, last_idx);
+        uint32_t number_of_elements = right_idx - left_idx;
+        uint32_t middle_idx = left_idx + (number_of_elements / 2);
+        printf("left: %d, mid = %d, right: %d\n", left_idx, middle_idx, right_idx);
 
         // Return if the target is found at the middle index
         if (arr[middle_idx] == target)
@@ -23,14 +23,14 @@ uint32_t binary_search(uint8_t arr[], uint32_t size, uint32_t target)
             break;
         }
         // Jump to right half, if the target is greater than the middle element
-        else if (target > arr[middle_idx])
+        if (target > arr[middle_idx])
         {
-            first_idx = middle_idx + 1;
+            left_idx = middle_idx + 1;
         }
         // Jump to left half, if the target is lesser than the middle element
-        else if (target < arr[middle_idx])
+        else
         {
-            last_idx = middle_idx - 1;
+            right_idx = middle_idx - 1;
         }
     }
     
